a820dea46a0db20f69a3243c20dec4566c646bff
swissChili <swisschili@fastmail.com>
2021-05-09 16:46:55 -0700

    Fix "if" condition

--- src/lisp/test.lisp
+++ src/lisp/test.lisp
@@ -1,7 +1,12 @@
 (defun two-plus-two ()
   (+ 2 2))
 
-(defun main ()
+(defun hmm-main ()
   (print "64 / (2 + 2) =")
   (print (/ 64 (two-plus-two)))
   (print nil))
+
+(defun main ()
+  (if t
+    (print 1)
+    (print 2)))
