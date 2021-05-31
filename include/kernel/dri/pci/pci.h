247cc56fbd3de03a775ff1bdc66b052eb97ed112
swissChili <swisschili@fastmail.com>
2021-05-30 20:55:13 -0700

    Identify PCI device classes

--- include/kernel/dri/pci/pci.h
+++ include/kernel/dri/pci/pci.h
@@ -9,6 +9,15 @@ enum
 	PCI_CONFIG_DATA = 0xCFC,
 };
 
+struct pci_device
+{
+	struct pci_vendor *vendor;
+	uchar class, subclass, prog_if;
+	ushort device_id;
+	bool valid;
+};
+
+// offset is in dwords
 uint pci_config_readd(uchar bus, uchar slot, uchar func, uchar offset);
-struct pci_vendor *pci_check_vendor(uchar bus, uchar slot, uchar func, ushort *v, ushort *d);
+struct pci_device pci_check_device(uchar bus, uchar slot, uchar func);
 void pci_print_devices();
